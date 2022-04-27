04aa12bb2ff6ea101e42a825d53153096ba4491e
Juan Hapes <juan@fizzmod.com>
2022-04-27 12:17:38 -0300

    Added tests

--- tests/wrappers/lambda-get.js
+++ tests/wrappers/lambda-get.js
@@ -1,18 +1,32 @@
-'use-strict';
+'use strict';
 
+const assert = require('assert');
 const sinon = require('sinon');
 
-const { Lambda, Handler } = require('@janiscommerce/lambda');
+const { Handler } = require('@janiscommerce/lambda');
 
 const Model = require('@janiscommerce/model');
+
+const { LambdaGet } = require('../../lib');
 
 describe('LambdaGet', () => {
 
 	const event = { session: { clientCode: 'defaultClient' } };
 
+	const stubGet = (items, totals) => {
+
+		sinon.stub(Model.prototype, 'get')
+			.resolves(items);
+
+		sinon.stub(Model.prototype, 'getTotals')
+			.resolves(totals);
+	};
+
 	afterEach(() => {
 		sinon.restore();
 	});
+
+	class InvalidGetWrapper extends LambdaGet {}
 
 	class ValidGetWrapper extends LambdaGet {
 
@@ -21,10 +35,94 @@ describe('LambdaGet', () => {
 		}
 	}
 
-	it('Should use model to get items and response', async() => {
+	it('Should reject when modelClass getter not found', async () => {
+		await assert.rejects(Handler.handle(InvalidGetWrapper, event), {
+			message: 'modelClass is required'
+		});
+	});
 
-		await Handler.handle(ValidGetWrapper, event);
+	it('Should use model to get items and response', async () => {
 
-	})
+		stubGet([], { total: 0 });
+
+		const response = await Handler.handle(ValidGetWrapper, event);
+
+		assert.deepEqual(response, {
+			items: [],
+			totals: { total: 0 }
+		});
+
+		sinon.assert.calledOnceWithExactly(Model.prototype.get, {});
+	});
+
+	it('Should get skus and response the items and totals with fields reducing result', async () => {
+
+		stubGet([{
+			id: '62685f1900b3e56803b9cf8c',
+			referenceId: 'coke-1lt',
+			name: 'Coke 1lt'
+		}, {
+			id: '62685f4d659ebb59d22893d7',
+			referenceId: 'coke-2lt',
+			name: 'Coke 2lt'
+		}], {
+			total: 2
+		});
+
+		const response = await Handler.handle(ValidGetWrapper, {
+			...event,
+			body: { fields: ['id', 'referenceId'] }
+		});
+
+		assert.deepEqual(response, {
+			items: [{
+				id: '62685f1900b3e56803b9cf8c',
+				referenceId: 'coke-1lt'
+			}, {
+				id: '62685f4d659ebb59d22893d7',
+				referenceId: 'coke-2lt'
+			}],
+			totals: { total: 2 }
+		});
+
+		sinon.assert.calledOnceWithExactly(Model.prototype.get, {});
+	});
+
+	it('Should get skus and response the items and totals using full params', async () => {
+
+		stubGet([{
+			id: '62685f1900b3e56803b9cf8c',
+			referenceId: 'coke-1lt',
+			name: 'Coke 1lt'
+		}], {
+			total: 1
+		});
+
+		const response = await Handler.handle(ValidGetWrapper, {
+			...event,
+			body: {
+				filters: { referenceId: 'coke-1lt' },
+				page: 1,
+				limit: 1,
+				order: { name: 'asc' }
+			}
+		});
+
+		assert.deepEqual(response, {
+			items: [{
+				id: '62685f1900b3e56803b9cf8c',
+				referenceId: 'coke-1lt',
+				name: 'Coke 1lt'
+			}],
+			totals: { total: 1 }
+		});
+
+		sinon.assert.calledOnceWithExactly(Model.prototype.get, {
+			filters: { referenceId: 'coke-1lt' },
+			page: 1,
+			limit: 1,
+			order: { name: 'asc' }
+		});
+	});
 
 });
